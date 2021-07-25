164c450eddfc84fe041971b3ac02459cad1eddd3
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2021-07-25 12:14:37 -0300

    Lista2-Exercicio16
    Faça um programa que receba o número de horas trabalhadas e o valor do salário
    mínimo, calcule e mostre o salário a receber, seguindo estas regras:
    a) a hora trabalhada vale a metade do salário mínimo.
    b) o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da
    hora trabalhada.
    c) o imposto equivale a 3% do salário bruto.
    d) o salário a receber equivale ao salário bruto menos o imposto.

--- Lista2-Exercicio16.cpp
+++ Lista2-Exercicio16.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-    /*FaÁa um programa que receba o n˙mero de horas trabalhadas e o valor do sal·rio
-    mÌnimo, calcule e mostre o sal·rio a receber, seguindo estas regras:
-    a) a hora trabalhada vale a metade do sal·rio mÌnimo.
-    b) o sal·rio bruto equivale ao n˙mero de horas trabalhadas multiplicado pelo valor da
+    /*Fa√ßa um programa que receba o n√∫mero de horas trabalhadas e o valor do sal√°rio
+    m√≠nimo, calcule e mostre o sal√°rio a receber, seguindo estas regras:
+    a) a hora trabalhada vale a metade do sal√°rio m√≠nimo.
+    b) o sal√°rio bruto equivale ao n√∫mero de horas trabalhadas multiplicado pelo valor da
     hora trabalhada.
-    c) o imposto equivale a 3% do sal·rio bruto.
-    d) o sal·rio a receber equivale ao sal·rio bruto menos o imposto.
+    c) o imposto equivale a 3% do sal√°rio bruto
+    d) o sal√°rio a receber equivale ao sal√°rio bruto menos o imposto.
     */
 
     float numhorastrab, salaminimo, salareceber, salabruto, horastrabvalor, imposto;
