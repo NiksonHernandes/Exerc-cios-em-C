7b786467b6ec1349e24edf7a329b445e9398264d
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2021-07-25 12:08:33 -0300

    Lista2-Exercicio13
     Sabe-se que:
    pé = 12 polegadas
    1 jarda = 3 pés
    1 milha = 1,760 jarda
    Faça um programa que receba uma medida em pés, faça as conversões a seguir
    e mostre os resultados.
    a) polegadas;
    b) jardas;
    c) milhas.

--- Lista2-Exercicio13.cpp
+++ Lista2-Exercicio13.cpp
@@ -4,10 +4,10 @@
 int main()
 {
     /*Sabe-se que:
-    pé = 12 polegadas
-    1 jarda = 3 pés
+    pÃ© = 12 polegadas
+    1 jarda = 3 pÃ©s
     1 milha = 1,760 jarda
-    Faça um programa que receba uma medida em pés, faça as conversões a seguir
+    FaÃ§a um programa que receba uma medida em pÃ©s, faÃ§a as conversÃµes a seguir
     e mostre os resultados.
     a) polegadas;
     b) jardas;
