2772ffbd79386e9b70860c5b7430cc1ef2e1c6c6
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2021-10-02 15:01:39 -0300

    Add files via upload

---
+++ Exercício_em_C_2º_Semestre/[Usando-Função]Lista-Vetores-Exercicio2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+    /*PREENCHER UM VETOR COM OS NUMEROS PARES DO NÚMERO 2 A 20*/
+
+    int vet[10], i, x;
+
+void vetor()
+{
+    for(i = 0; i<10; i++)
+    {
+        vet[i] = x + 2;
+        x = x + 2;
+    }
+}
+
+void exibir()
+{
+    for(i=0; i<10; i++)
+    {
+        printf("Os valores [%i] são: %i\n", i, vet[i]);
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    vetor();
+    exibir();
+
+    return 0;
+}
